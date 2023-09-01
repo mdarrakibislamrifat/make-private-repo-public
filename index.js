dc991eaa7390b7f43c7a3438759e92e00901d16f
unknown <rohanrifat43@gmail.com>
2023-09-01 12:31:00 +0600

    new6

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const apiLoad = async () => {
   const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
   const data = await response.json();
+  // if(data==''){
+
+  // }
   
 
   const categoryContainer = document.getElementById('category-container');
@@ -24,26 +27,36 @@ const handleLoadData = async (categoryId) => {
 
   data.data.forEach((card) => {
     card.authors.map(profile=>{
-      console.log(profile.profile_picture)
+     
     
     const div = document.createElement('div');
-    div.classList = "card  bg-gray-100 shadow-xl";
+    div.classList = "card p-5 bg-gray-100 shadow-xl";
+    
 
     div.innerHTML = `
    
                 
-                <figure><img class="h-60 w-96" src=${card?.thumbnail} alt="Shoes" />
+                <figure class="h-60 w-96" ><img  src=${card?.thumbnail} alt="Shoes" />
                 </figure>
                 
                 <div class="card-body">
                   <h2 class="card-title">${card.title}</h2>
-                  <div class="flex justify-around">
+
+                  <div class="flex justify-items-center">
                  <img class="rounded-full w-14 h-14" src=${profile.profile_picture}/>
-                  <p>${profile.profile_name}</p>
-                  </div>
-                    <p>${profile.profile_name}</p>
+
+                  <p class="text-xl font-normal mt-2 ml-3">${profile.profile_name}</p>
                   
-                  <p>Views :${card?.others?.views}</p>
+                 ${profile.verified==true ?
+                
+                 '<img src="fi_10629607.svg"/>': ' '
+                }
+                 
+                  </div>
+
+                    
+                  
+                  <p>Views : ${card?.others?.views}</p>
                   
                 </div>
              
