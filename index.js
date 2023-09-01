baabf763127ae3041bb77ec8db775e8f76857bef
unknown <rohanrifat43@gmail.com>
2023-09-01 11:20:31 +0600

    new5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const apiLoad = async () => {
   const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
   const data = await response.json();
+  
 
   const categoryContainer = document.getElementById('category-container');
   data.data.forEach((category) => {
@@ -19,26 +20,29 @@ const handleLoadData = async (categoryId) => {
   const data = await response.json();
 
   const cardContainer = document.getElementById('card-container');
-  cardContainer.innerHTML=" "
+  cardContainer.innerHTML=" ";
+
   data.data.forEach((card) => {
-    console.log(card.authors)
+    card.authors.map(profile=>{
+      console.log(profile.profile_picture)
+    
     const div = document.createElement('div');
     div.classList = "card  bg-gray-100 shadow-xl";
 
     div.innerHTML = `
    
                 
-                <figure><img src=${card?.thumbnail} alt="Shoes" />
+                <figure><img class="h-60 w-96" src=${card?.thumbnail} alt="Shoes" />
                 </figure>
                 
                 <div class="card-body">
                   <h2 class="card-title">${card.title}</h2>
-                  <div class="flex">
-                  <img src=${card?.profile_picture} />
-                  <p>${card?.profile_name}</p>
-                    
-                    <p>${card.verified}</p>
+                  <div class="flex justify-around">
+                 <img class="rounded-full w-14 h-14" src=${profile.profile_picture}/>
+                  <p>${profile.profile_name}</p>
                   </div>
+                    <p>${profile.profile_name}</p>
+                  
                   <p>Views :${card?.others?.views}</p>
                   
                 </div>
@@ -46,10 +50,12 @@ const handleLoadData = async (categoryId) => {
     `;
     cardContainer.appendChild(div)
   })
+  })
 
 }
 apiLoad();
 handleLoadData('1000');
+
 const description = () => {
   window.location.href = "description.html";
 
